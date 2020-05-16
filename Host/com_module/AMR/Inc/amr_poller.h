c3fe796114eede8fddab6ddc99af2accd0bc54d3
Fiames <fiamesgithub@gmail.com>
2020-05-16 22:44:04 +0300

    Added poller, serial and timer tests

---
+++ Host/com_module/AMR/Inc/amr_poller.h
@@ -0,0 +1,37 @@
+/*
+ * amr_poller.h
+ *
+ *  Created on: May 9, 2020
+ *      Author: serbay.ozkan
+ */
+
+#ifndef INC_AMR_POLLER_H_
+#define INC_AMR_POLLER_H_
+
+#include <stdint.h>
+
+#define AMR_MAX_NO_OF_METER     ( 15 )
+
+typedef struct
+{
+	uint8_t selected_meter;
+	uint32_t serial_no[AMR_MAX_NO_OF_METER];
+	char model[AMR_MAX_NO_OF_METER];
+}amr_poller_t;
+
+typedef enum
+{
+	INIT,
+	START,
+	READOUT
+}amr_poller_state_t;
+
+void amr_create_start_message(uint32_t serial_number, char* start_message, char meter_type);
+void amr_create_readout_message(char* readout_message);
+void amr_update_selected_meter(uint8_t meter_id);
+amr_poller_state_t amr_get_poller_state(void);
+
+extern amr_poller_t amr_poller;
+extern amr_poller_state_t amr_poller_state;
+
+#endif /* INC_AMR_POLLER_H_ */
---
+++ Host/com_module/AMR/Test/amr_poller_test.cpp
@@ -0,0 +1,68 @@
+/*
+ * amr_poller_test.cpp
+ *
+ *  Created on: May 9, 2020
+ *      Author: serbay.ozkan
+ */
+
+#include "CppUTest/TestHarness.h"
+
+#include <string.h>
+
+extern "C"
+{
+#include "amr_poller.h"
+}
+
+TEST_GROUP(amr_poller)
+{
+	void setup()
+	{
+
+	}
+
+	void teardown()
+	{
+
+	}
+};
+
+TEST(amr_poller, createStartMessage)
+{
+	uint32_t serial_number = 123456789;
+	char start_message[20];
+	char meter_type = 'M';
+
+	amr_create_start_message(serial_number, start_message, meter_type);
+
+	STRCMP_EQUAL("/?MSY123456789!\r\n", (char*)start_message);
+}
+
+TEST(amr_poller, createReadoutMessage)
+{
+	char readout_message[4];
+	memset(readout_message, 0, sizeof(readout_message));
+
+	char readout_message_actual[4] = {0x06, 0x30, 0x35, 0x30};
+
+	amr_create_readout_message(readout_message);
+
+	for (int idx = 0; idx < 4; idx++)
+	{
+		BYTES_EQUAL(readout_message_actual[0], readout_message[0]);
+	}
+}
+
+TEST(amr_poller, updateSelectedMeter)
+{
+	uint8_t meter_id = 1;
+
+	amr_update_selected_meter(meter_id);
+
+	CHECK_EQUAL(meter_id, amr_poller.selected_meter);
+}
+
+TEST(amr_poller, updatePollerState)
+{
+	CHECK_EQUAL(INIT, amr_get_poller_state());
+}
